9a9a5747edab29e87a62335a558d8246c9a88a6a
hmuronaka <capriccio27@gmail.com>
2015-06-02 21:45:43 +0900

    Initial Commit

---
+++ BrightnessSensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrightnessSensor
+//
+//  Created by Muronaka Hiroaki on 2015/06/02.
+//  Copyright (c) 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
